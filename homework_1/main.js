d15c5c424847cb0f13a52289ed5069a172f83493
Oleh3131 <oshevskyioleh@gmail.com>
2022-07-21 23:57:44 +0300

    first add information

---
+++ homework_1/main.js
@@ -0,0 +1,202 @@
+
+
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//  Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+
+
+// let greeting = 'hello';
+//
+// console.log(greeting);
+//
+// alert(greeting);
+//
+// document.write(`<h2>${greeting}</h2>`)
+//
+// document.write(`<hr/>`)
+//
+// let mainName = 'owu';
+//
+// console.log(mainName);
+//
+// alert(mainName);
+//
+// document.write(`<h2>${mainName}</h2>`);
+//
+// document.write(`<hr/>`)
+//
+// let upperDomain = 'com';
+//
+// console.log(upperDomain);
+//
+// alert(upperDomain);
+//
+// document.write(`<h2>${upperDomain}</h2>`)
+//
+// document.write(`<hr/>`)
+//
+// let oneLove = 'ua';
+//
+// console.log(oneLove);
+//
+// alert(oneLove);
+//
+// document.write(`<h2>${oneLove}</h2>`)
+//
+// document.write(`<hr/>`)
+//
+// let numberOne = 1;
+//
+// console.log(numberOne);
+//
+// alert(numberOne);
+//
+// document.write(`<h2>${numberOne}</h2>`)
+//
+// document.write(`<hr/>`)
+//
+// let numberTwo = 10;
+//
+// console.log(numberTwo);
+//
+// alert(numberTwo);
+//
+// document.write(`<h2>${numberTwo}</h2>`)
+//
+// document.write(`<hr/>`)
+//
+// let numberThree = -999;
+//
+// console.log(numberThree);
+//
+// alert(numberThree);
+//
+// document.write(`<h2>${numberThree}</h2>`)
+//
+// document.write(`<hr/>`)
+//
+// let numberFour = 123;
+//
+// console.log(numberFour);
+//
+// alert(numberFour);
+//
+// document.write(`<h2>${numberFour}</h2>`)
+//
+// document.write(`<hr/>`)
+//
+// const pi = 3.14;
+//
+// console.log(pi);
+//
+// alert(pi);
+//
+// document.write(`<h2>${pi}</h2>`)
+//
+// document.write(`<hr/>`)
+//
+// let numberFive = 16;
+//
+// console.log(numberFive);
+//
+// alert(numberFive);
+//
+// document.write(`<h2>${numberFive}</h2>`);
+//
+// document.write(`<hr/>`)
+//
+// let numberSix = 2.7;
+//
+// console.log(numberSix);
+//
+// alert(numberSix);
+//
+// document.write(`<h2>${numberSix}</h2>`);
+//
+// document.write(`<hr/>`)
+//
+// const booleanOne = true;
+//
+// console.log(booleanOne);
+//
+// alert(booleanOne);
+//
+// document.write(`<h2>${booleanOne}</h2>`);
+//
+// document.write(`<hr/>`);
+//
+// const booleanTwo = false;
+//
+// console.log(booleanTwo)
+//
+// alert(booleanTwo);
+//
+// document.write(`<h2>${booleanTwo}</h2>`)
+
+
+
+
+
+
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ.
+// З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+
+
+
+
+
+// let firstName = 'Roman';
+//
+// let middleName = 'Volodymyrovych';
+//
+// let lastName = 'Shevchenko';
+//
+// let person = lastName + ' ' + firstName + ' ' + middleName;
+//
+// // let person = `${lastName} ${firstName} ${middleName}`;
+//
+// console.log(person);
+//
+// document.write(`<h2>${person}</h2>`);
+
+
+
+
+
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+
+
+
+// let a = 100;
+//
+// console.log(typeof a)
+//
+// let b = '100';
+//
+// console.log(typeof b)
+//
+// let c = true;
+//
+// console.log(typeof c)
+
+
+
+
+
+
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+
+
+// let name = prompt('What is your name?');
+//
+// let surname = prompt('What is your surname?');
+//
+// let age = prompt('How old are you?');
+//
+// let information = `My name is ${name} ${surname}.I am ${age}.`
+//
+// console.log(information);
+//
+// document.write(`<h2>${information}</h2>`)

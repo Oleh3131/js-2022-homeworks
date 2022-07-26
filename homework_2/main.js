4aceb1b7e6a655e362a67b5ea9751e5f99dd2ffc
Oleh3131 <oshevskyioleh@gmail.com>
2022-07-27 00:16:59 +0300

    first add info

---
+++ homework_2/main.js
@@ -0,0 +1,219 @@
+
+
+
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль.................................................................................
+
+
+// let someArray = ['Java', 'Python', 3.14, ['HTML', [331, 'Your profile'], 'CSS', true], 31, false, 1996, -0.12, 'NodeJs', [undefined, 'PHP', false, null]]
+
+
+// console.log(someArray[0]);
+// console.log(someArray[1]);
+// console.log(someArray[2]);
+// console.log(someArray[4]);
+// console.log(someArray[5]);
+// console.log(someArray[6]);
+// console.log(someArray[7]);
+// console.log(someArray[8]);
+//
+// let secondArray = someArray[3];
+// console.log(secondArray);
+// console.log(secondArray[0]);
+// console.log(secondArray[2]);
+// console.log(secondArray[3]);
+//
+// let thirdArray = secondArray[1];
+// console.log(thirdArray);
+// console.log(thirdArray[0]);
+// console.log(thirdArray[1]);
+//
+// let fourthArray = someArray[9];
+// console.log(fourthArray);
+// console.log(fourthArray[0]);
+// console.log(fourthArray[1]);
+// console.log(fourthArray[2]);
+// console.log(fourthArray[3]);
+
+
+
+
+
+
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.....................................................................................................
+
+
+// let firstBookJackLondon = {
+//
+//     title: 'White Fang',
+//     pageCount: 240,
+//     genre: 'Novel, Adventure fiction literature'
+//
+// };
+//
+// console.log(firstBookJackLondon);
+//
+// let secondBookOfJackLondon = {
+//
+//     title: 'Martin Eden',
+//     pageCount: 580,
+//     genre: 'Fiction novel'
+//
+// };
+//
+// console.log(secondBookOfJackLondon);
+//
+// let thirdBookOfJackLondon = {
+//
+//     title: 'Sea wolf',
+//     pageCount: 387,
+//     genre: 'An adventure novel'
+//
+// }
+//
+// console.log(thirdBookOfJackLondon);
+
+
+
+
+
+
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors.
+// Поле "автори" - масив. Кожен автор має поля name,age.........................................................................................................................................
+
+
+// let firstBookDescription = {
+//
+//     title: 'Green Mile',
+//     pageCount: 496,
+//     genre: 'Horror literature, Feuilleton novel, Magic realism, Dark fantasy, Fantasy, Southern gothic, Fiction',
+//     author: [
+//         {
+//             name: 'Stephen Edwin King',
+//             age: 74
+//         }
+//     ]
+// }
+//
+// console.log(firstBookDescription);
+// console.log(firstBookDescription.author)
+// console.log(firstBookDescription.author[0].name)
+// console.log(firstBookDescription.author[0]['age'])
+//
+// let secondBookDescription = {
+//
+//     title: 'The Old Man and the Sea',
+//     pageCount: 385,
+//     genre: 'Romance, Allegory, Nautical Fiction',
+//     author: [
+//         {
+//             name: 'Ernest Miller Hemingway',
+//             age: 61
+//         }
+//     ]
+// }
+//
+// console.log(secondBookDescription);
+// console.log(secondBookDescription.author)
+// console.log(secondBookDescription.author[0].name)
+// console.log(secondBookDescription.author[0]['age'])
+//
+// let thirdBookDescription = {
+//
+//     title: 'Rob Roy',
+//     pageCount: 587,
+//     genre: 'Novel, Historical fiction, Historical genre',
+//     author: [
+//         {
+//             name: 'Walter Scott',
+//             age: 61
+//         }
+//     ]
+// }
+//
+// console.log(thirdBookDescription);
+// console.log(thirdBookDescription.author)
+// console.log(thirdBookDescription.author[0].name)
+// console.log(thirdBookDescription.author[0]['age'])
+
+
+
+
+
+// - Створити масив з 10 об'єктами які описують сутніть "користувач".
+// Поля: name, username,password. Вивести в консоль пароль кожного користувача......................................................................................................................
+
+
+// const users = [
+//
+//     {
+//         name: 'Marco',
+//         username: 'Bootop_21',
+//         password: 'erf45erf712etr'
+//     },
+//     {
+//         name: 'Georgia',
+//         username: 'Mellowor47',
+//         password: 'EREW265cssf6'
+//     },
+//     {
+//         name: 'Nick',
+//         username: 'Epicin_947',
+//         password: 'wftDGX1215ser'
+//     },
+//     {
+//         name: 'Donna',
+//         username: 'ToyDari_265',
+//         password: 'dvd565FRGTR'
+//     },
+//     {
+//         name: 'Pete',
+//         username: 'Iamaugar_215',
+//         password: 'wERWR26vfdf'
+//     },
+//     {
+//         name: 'Lena',
+//         username: 'FunkWel+877',
+//         password: 'ver54541tryr'
+//     },
+//     {
+//         name: 'Joe',
+//         username: 'NeilInca_97',
+//         password: '26ver265te32etE'
+//     },
+//     {
+//         name: 'Brad',
+//         username: 'MonsterDrummer_97',
+//         password: 'VRETT454ERT6545'
+//     },
+//     {
+//         name: 'Myrtle',
+//         username: 'SlimStrong_9742',
+//         password: 'erte26vtrt'
+//     },
+//     {
+//         name: 'Abraham',
+//         username: 'Unisebobb_97',
+//         password: 'er1351v0ty023srt'
+//     }
+//
+// ];
+//
+//
+// let usersPassword = [
+//
+//     users[0].password,
+//     users[1].password,
+//     users[2].password,
+//     users[3].password,
+//     users[4].password,
+//     users[5].password,
+//     users[6].password,
+//     users[7].password,
+//     users[8].password,
+//     users[9].password
+//
+// ];
+//
+// console.log(usersPassword);
